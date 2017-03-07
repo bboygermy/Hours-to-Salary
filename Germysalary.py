cc1bef582e616a591f9cfd418cc4cab242b0593f
Jeremy Cheung <jeremyccheung@yahoo.com>
2017-03-06 18:28:41 -0600

    Update Germysalary.py

--- Germysalary.py
+++ Germysalary.py
@@ -1,8 +1,13 @@
 #This program will convert an hourly pay rate into an annual salary
 #This program assumes a 40 hour work week paid for 52 weeks of the year (in USD)
+
 hours=40
 aweeks=52
-query=raw_input("Please enter your hourly pay rate: $")
-rate=float(query)
+try:
+  query=raw_input("Please enter your hourly pay rate: $")
+  rate=float(query)
+except:
+  print "Please enter your hourly rate in Arabic numerals."
+  quit()
 salary=hours*aweeks*rate
 print "Your salary is $","%.2f" % salary
